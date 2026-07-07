fdc79e4cdb169b5bc36165c80df47d19da329210
Manickam <manick.badsha@gmail.com>
2026-07-07 13:00:40 -0400

    new branch and pull request

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -15,7 +15,7 @@ test('get started link', async ({ page }) => {
 
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+}); //Test
 
 test('get started link2', async ({ page }) => {
   await page.goto('https://playwright.dev/');
